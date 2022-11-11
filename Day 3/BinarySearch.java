6b8b94dbb167715783221fddb2ffff8f8a16c5a6
Gajal21 <115548474+Gajal21@users.noreply.github.com>
2022-11-12 02:08:33 +0530

    Create BinarySearch.java

---
+++ Day 3/BinarySearch.java
@@ -0,0 +1,22 @@
+class Solution {
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        int s=0;
+        int e=n-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(arr[mid]==k){
+                return mid;
+            }
+            else if(arr[mid]>k){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+        }
+        return -1;
+    }
+}
+// T.C. ->O(log n)
+// S.C. ->O(1)
